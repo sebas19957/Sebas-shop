5d1e01b6cd6044bbeb8a6fa3c12e4cdb7bb7437f
Analista 1 <analista1@MERCADEOVIRTUAL.local>
2022-12-19 17:27:58 -0500

    Implementa la autentificacón de usuarios, falta terminar

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,38 +1,82 @@
 import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { AuthLayout } from '../../components/layouts'
 
 const LoginPage = () => {
+
+  type FormData = {
+    email: string,
+    password: string,
+  };
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+  const onLoginUser = (data: FormData) => {
+    console.log({ data });
+  }
+
   return (
     <AuthLayout title={'Ingresar'}>
-      <Box sx={{ width: 350, padding: '10px 20px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box sx={{ width: 350, padding: '10px 20px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant='h1' component="h1">Iniciar Sesión</Typography>
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label="Contraseña" type='password' variant="filled" fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Correo"
+                type='email'
+                variant="filled"
+                fullWidth
+                {
+                ...register('email', {
+                  required: 'Este campo es requerido'
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type='password'
+                variant="filled"
+                fullWidth
+                {...register('password', {
+                  required: 'Este campo es requerido',
+                  minLength: { value: 6, message: 'Mínimo 6 caracteres' }
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <Button color="secondary" className='circular-btn' size='large' fullWidth>
-              Ingresar
-            </Button>
-          </Grid>
+            <Grid item xs={12}>
+              <Button
+                type='submit'
+                color="secondary"
+                className='circular-btn'
+                size='large'
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
 
-          <Grid item xs={12} display='flex' justifyContent='end'>
-            <NextLink href="/auth/register" passHref>
-              <Link underline='always'>
-                ¿No tienes cuenta?
-              </Link>
-            </NextLink>
+            <Grid item xs={12} display='flex' justifyContent='end'>
+              <NextLink href="/auth/register" passHref>
+                <Link underline='always'>
+                  ¿No tienes cuenta?
+                </Link>
+              </NextLink>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   )
 }
